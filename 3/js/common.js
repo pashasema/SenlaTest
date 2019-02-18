ed8b17e447dbba2cf7834238ee3c428fbc1380c1
pasha.semenov <dosurgeest@gmail.com>
2019-02-18 18:05:38 +0300

    add fixes to 3 project

--- 3/js/common.js
+++ 3/js/common.js
@@ -3,14 +3,27 @@ $(function() {
 });
 
 function shortestWord(){
-
+	$("#input_text").keypress(function(event){
+		var ew = event.which;
+		if(ew == 32)
+			return true;
+		if(48 <= ew && ew <= 57)
+			return true;
+		if(65 <= ew && ew <= 90)
+			return true;
+		if(97 <= ew && ew <= 122)
+			return true;
+		if(1040 <= ew && ew <= 1105)
+			return true;
+		return false;
+	});
 	$('#input_submit').on('click', function(){
 		$('p').empty();
 		let valueInput = $('#input_text').val();
 		let words= valueInput.split(' ');
 		let smallestWord = Infinity;
 		for (let i=0; i<words.length ; i++) {
-			if(smallestWord>words[i].length){
+			if(smallestWord>words[i].length && words[i]!=''){
 				smallestWord = words[i].length;
 			};
 		}
